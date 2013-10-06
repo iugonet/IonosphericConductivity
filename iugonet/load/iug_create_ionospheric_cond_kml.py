fe4c058736bde9b66c70c888de1c479468390254
Yukinobu KOYAMA <ykoyama@kugi.kyoto-u.ac.jp>
2013-10-06 14:23:01 +0900

    Fixed

---
+++ iugonet/load/iug_create_ionospheric_cond_kml.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+#
+# sudo apt-get install python-pip
+# pip install pykml
+#
+
+import os
+import sqlite3
+from lxml import etree
+from pykml.parser import Schema
+from pykml.factory import KML_ElementMaker as KML
+from pykml.factory import GX_ElementMaker as GX
+
+conn = sqlite3.connect(os.environ['UDASPLUS_HOME']+'/iugonet/load/iug_ionospheric_cond.db')
+
+sql = "select * from iug_ionospheric_cond;"
+#conn.execute(sql)
+conn.row_factory= sqlite3.Row
+for row in conn.execute(sql):
+    print row['sigma_0'], row['sigma_1'], row['sigma_2'], row['sigma_xx'], row['sigma_yy'], row['sigma_xy'], row['height'], row['glat'], row['glon'], row['yyyy'], row['mmdd'], row['ltut'], row['atime'], row['algorithm']
+conn.close()
+
