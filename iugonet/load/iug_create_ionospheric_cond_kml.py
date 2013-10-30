b50802a37ac874afe011eaaa7225cba71b7aed51
Yukinobu KOYAMA <ykoyama@kugi.kyoto-u.ac.jp>
2013-10-30 16:40:40 +0900

    Fixed

--- iugonet/load/iug_create_ionospheric_cond_kml.py
+++ iugonet/load/iug_create_ionospheric_cond_kml.py
@@ -20,6 +20,30 @@ def prettify(elem):
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
 
+class Hoge:
+    
+    def __init__(self, yyyy, mmdd, ltut, atime):
+        self.yyyy = yyyy
+        self.mmdd = mmdd
+        self.ltut = ltut
+        self.atime = atime
+
+def get_file_names():
+    conn = sqlite3.connect(os.environ['UDASPLUS_HOME']+'/iugonet/load/ionospheric_cond.db')
+    sql = "select distinct yyyy,mmdd,ltut,atime,algorithm from ionospheric_cond;"
+    cursor = conn.execute(sql)
+    result = cursor.fetchall()
+
+    hogeList = []
+
+    for row in result:
+        hogeList.append(Hoge(row[0], row[1], row[2], row[3]))
+
+    cursor.close()
+    conn.close;
+
+    return hogeList
+
 def get_max():
     conn = sqlite3.connect(os.environ['UDASPLUS_HOME']+'/iugonet/load/ionospheric_cond.db')
 
@@ -54,20 +78,44 @@ def get_max():
     for row in c:
         print row
         
-    conn.close;
-
-def read_all():
-    conn = sqlite3.connect(os.environ['UDASPLUS_HOME']+'/iugonet/load/ionospheric_cond.db')
-
-    sql = "select * from ionospheric_cond;"
-    c = conn.execute(sql)
-#    conn.row_factory = sqlite3.Row
-    for row in c:
-        print row
-
     conn.close
-    
 
+def generateKml(value):
+    print value.yyyy, value.mmdd, value.ltut, value.atime
+
+
+#    get_max()
+
+    kml = etree.Element('kml')
+    kml.set('xmlns','http://www.opengis.net/kml/2.2')
+                                  
+    document = etree.SubElement(kml, 'Document')
+    folder1 = etree.SubElement(document, 'Folder')
+    name1 = etree.SubElement(folder1, 'name')
+    name1.text = ' Alt.:'+'km, range='+'km '
+    folder2 = etree.SubElement(folder1, 'Folder')
+    name2 = etree.SubElement(folder2, 'name')
+    name2.text = ' Lat:'+'deg.'+' Lon:'+'deg.'
+    timestamp = etree.SubElement(folder2, 'TimeStamp')
+    when = etree.SubElement(timestamp, 'when')
+    placemark = etree.SubElement(folder2, 'Placemark')
+    name3 = etree.SubElement(placemark, 'name')
+    name3.text = 'Polygon'
+    style = etree.SubElement(placemark, 'Style')
+    polystyle = etree.SubElement(style, 'PolyStyle')
+    color = etree.SubElement(polystyle, 'color')
+    color.text = 'ffff0040'
+    outline = etree.SubElement(polystyle, 'outline')
+    outline.text = '0'
+    polygon = etree.SubElement(placemark, 'Polygon')
+    altitudeMode = etree.SubElement(polygon, 'altitudeMode')
+    altitudeMode.text = 'absolute'
+    outerBoundaryIs = etree.SubElement(polygon, 'outerBoundaryIs')
+    linearRing = etree.SubElement(outerBoundaryIs, 'LinearRing')
+    coordinates = etree.SubElement(linearRing, 'coordinates')
+    coordinates.text = '\n122.100,25.0500,81000.0\n122.100,25.9500,81000.0\n123.900,25.9000,81000.0\n123.900,25.0500,81000.0'
+    
+    return prettify(kml)
 
 def main():
 #    conn = sqlite3.connect(os.environ['UDASPLUS_HOME']+'/iugonet/load/ionospheric_cond.db')
@@ -78,35 +126,19 @@ def main():
 #    for row in conn.execute(sql):
 #        print row['sigma_0'], row['sigma_1'], row['sigma_2'], row['sigma_xx'], row['sigma_yy'], row['sigma_xy'], row['height'], row['glat'], row['glon'], row['yyyy'], row['mmdd'], row['ltut'], row['atime'], row['algorithm']
 #        conn.close()
+    hogeList = get_file_names()
+    for value in hogeList:
+        if value.ltut == 0:
+            str_ltut = 'LT'
+        elif value.ltut ==1:
+            str_ltut = 'UT'
 
-    kml = etree.Element('kml')
-    kml.set('xmlns','http://www.opengis.net/kml/2.2')                                  
-    document = etree.SubElement(kml, 'Document')
-    name = etree.SubElement(document, 'name')
-    name.text = 'name'
-    open = etree.SubElement(document, 'open')
-    open.text = '0'
-    PlaceMark = etree.SubElement(document, 'PlaceMark')
-    PlaceMarkName = etree.SubElement(PlaceMark, 'name')
-    Polygon = etree.SubElement(PlaceMark, 'Polygon')
-    attributeMode = etree.SubElement(Polygon, 'attributeMode')
-    attributeMode.text = 'absolute'
-    outerBoundaryIs = etree.SubElement(Polygon, 'outerBoundaryIs')
-    linearRing = etree.SubElement(outerBoundaryIs, 'linearRing')
-    coordinates = etree.SubElement(linearRing, 'coordinates')
-    coordinates.text = '0,0,0\n10,0,0\n10,10,0\n,0,10,0'
-#
-    Style = etree.SubElement(PlaceMark, 'Style')
-    PolyStyle = etree.SubElement(Style, 'PolyStyle')
-    color = etree.SubElement(PolyStyle, 'color')
-    color.text = 'a0ff00ff'
-    outline = etree.SubElement(PolyStyle, 'outline')
-    outline.text = '0'
-    print prettify(kml)
+        filename = 'tomogra_'+str(value.yyyy)+str(value.mmdd).rjust(4,"0")+'_'+str(value.atime)+str_ltut+'.kml'
 
-    get_max()
-    read_all()
+        f = open(filename, 'w')
+        str_kml = generateKml(value)
+        f.write(str_kml)
+        f.close()
 
 if __name__ == '__main__':
     main()
-
